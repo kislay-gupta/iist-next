441345fead1dbcfca4e28021dda2f2a12e31ddd8
kislay gupta <kislaygupta007@gmail.com>
2025-02-02 16:38:23 +0530

    feat: tutorial page

--- src/app/(root)/tutorials/page.tsx
+++ src/app/(root)/tutorials/page.tsx
@@ -2,7 +2,13 @@ import TextAnimation from "@/components/animations/TextAnimation";
 import TutorialComponent from "@/components/tutorials/TutorialComponent";
 import Link from "next/link";
 import React from "react";
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+    title: 'Tutorial || IIST, Bihar',
+    description:
+        "Learn everything about IIST, Bihar – courses, admissions, eligibility, fees, and career opportunities. Get a complete step-by-step tutorial for aspiring students!",
+}
 const Tutorials = () => {
     return (
         <div className="lg:w-11/12 mx-auto px-4 py-8">
--- src/components/tutorials/TutorialComponent.tsx
+++ src/components/tutorials/TutorialComponent.tsx
@@ -2,40 +2,47 @@ import React from 'react'
 import AnimatedGrid from '../animations/AnimatedGrid'
 import { TutorialsCard } from '../cards/';
 interface ProjectCardProps {
-    title?: string;
-    image?: string;
+    CatName?: string;
+    imageLink?: string;
     slug?: string;
     isLoading?: boolean;
     category?: "projects" | "blog";
 }
-const projectCards: ProjectCardProps[] = [
-    { title: "Arduino Projects", category: "projects", slug: "arduino-projects" },
-    { title: "IoT Innovations", category: "projects", slug: "iot-innovations" },
-    { title: "Web Development Essentials", category: "projects", slug: "web-development-essentials" },
-    { title: "Frontend Magic", category: "projects", slug: "frontend-magic" },
-    { title: "Backend Solutions", category: "projects", slug: "backend-solutions" },
-    { title: "HTML Basics", category: "blog", slug: "html-basics" },
-    { title: "CSS Styling Tips", category: "blog", slug: "css-styling-tips" },
-    { title: "Tailwind Mastery", category: "blog", slug: "tailwind-mastery" },
-    { title: "JavaScript Techniques", category: "blog", slug: "javascript-techniques" },
-    { title: "Python Power", category: "projects", slug: "python-power" },
-    { title: "PHP Development", category: "projects", slug: "php-development" },
-    { title: "TypeScript Advantage", category: "blog", slug: "typescript-advantage" },
-    { title: "React.js Fundamentals", category: "projects", slug: "reactjs-fundamentals" },
-    { title: "Next.js for Professionals", category: "projects", slug: "nextjs-for-professionals" },
-    { title: "Bootstrap Design", category: "blog", slug: "bootstrap-design" },
-];
+import conf from "@/config/config";
 
-const TutorialComponent = () => {
+async function getTutorials() {
+    try {
+        const res = await fetch(
+            `${conf.baseUrl}/tutorial?req_data=getTutCat`,
+            {
+                next: { revalidate: 60 }, // Revalidate cache every 60 seconds
+            }
+        );
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        return data?.data || [];
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        return [];
+    }
+}
+
+const TutorialComponent = async () => {
+    const projectCards = await getTutorials()
+
     return (
         <div className="lg:container w-full  lg:my-16 mx-auto">
             <AnimatedGrid>
                 {projectCards?.map((category: ProjectCardProps) => (
                     <TutorialsCard
-                        key={category.title}
-                        image={"https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+                        key={category.CatName}
+                        image={category.imageLink}
                         slug={category.slug}
-                        title={category.title}
+                        title={category.CatName}
                         isLoading={false}
 
                     />
