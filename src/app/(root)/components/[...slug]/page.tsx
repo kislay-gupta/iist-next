93ae07673006a4623dbca548424833df31a9271a
kissslayyy <kislaygupta007@gmail.com>
2025-02-21 23:48:06 +0530

    feat: color changed

--- src/app/(root)/components/[...slug]/page.tsx
+++ src/app/(root)/components/[...slug]/page.tsx
@@ -27,47 +27,46 @@ interface ProductCardProps {
   price?: number;
   DiscPrice?: number;
   slug: string;
-
 }
-
 
 const getComPonentsByCategoru = async (slug: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}componentData?req_data=CompByCatSlugOrID&CompCatSlug=${slug}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}componentData?req_data=CompByCatSlugOrID&CompCatSlug=${slug}`
+  );
   const data = await res.json();
   return data;
-}
+};
 
-export default async function Page({
-  params,
-}: PageParams) {
-  const resolvedParams = await (params);
+export default async function Page({ params }: PageParams) {
+  const resolvedParams = await params;
   const data = await getComPonentsByCategoru(resolvedParams.slug[0]);
 
-  return <>
-    <div className="max-sm:w-full lg:w-11/12 mx-auto px-4 py-8">
-      <nav className="mb-8 flex items-center gap-2 text-sm text-gray-600">
-        <Link href="/" className="text-green-600 hover:underline">
-          Home
-        </Link>
-        <span>/</span>
-        <span>{resolvedParams.slug[0]}</span>
-      </nav>
-      <TextAnimation text={resolvedParams.slug[0]} />
-      <div className="lg:container w-full  lg:my-16 mx-auto">
-        <AnimatedGrid className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2">
-          {data?.data?.map((category: ProductCardProps) => (
-            <ComponentsCard
-              key={category.sno}
-              image={category.imageLink}
-              slug={category.slug}
-              title={category.name}
-
-              originalPrice={Number(category.price)}
-              price={Number(category.DiscPrice)}
-            />
-          ))}
-        </AnimatedGrid>
+  return (
+    <>
+      <div className="max-sm:w-full lg:w-screen   px-4 py-8">
+        <nav className="mb-8 flex items-center gap-2 text-sm text-gray-600">
+          <Link href="/" className="text-green-600 hover:underline">
+            Home
+          </Link>
+          <span>/</span>
+          <span>{resolvedParams.slug[0]}</span>
+        </nav>
+        <TextAnimation text={resolvedParams.slug[0]} />
+        <div className="lg:container w-full  lg:my-16 mx-auto">
+          <AnimatedGrid className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2">
+            {data?.data?.map((category: ProductCardProps) => (
+              <ComponentsCard
+                key={category.sno}
+                image={category.imageLink}
+                slug={category.slug}
+                title={category.name}
+                originalPrice={Number(category.price)}
+                price={Number(category.DiscPrice)}
+              />
+            ))}
+          </AnimatedGrid>
+        </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 }
